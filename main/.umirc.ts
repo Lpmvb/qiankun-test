55f4189a96532ad1113ec17759cbcc556ecce1d9
Lpmvb <lpmvbs@gmail.com>
2020-05-25 13:55:15 +0800

    update router

--- main/.umirc.ts
+++ main/.umirc.ts
@@ -11,11 +11,40 @@ export default defineConfig({
           name: 'app1', // 唯一 id
           entry: 'http://localhost:7001', // html entry
           base: '/app1', // sys 的路由前缀，通过这个前缀判断是否要启动该应用，通常跟子应用的 base 保持一致
+          mountElementId: 'root-subapp-container',
         },
-      ]
+      ],
+      // defer: true,
     }
   },
   routes: [
-    { path: '/', component: '@/pages/index' },
+    {
+      path: '/',
+      routes: [
+        {
+          path: '/',
+          component: '../layouts/BasicLayout',
+          routes: [
+            {
+              path: '/',
+              redirect: '/home',
+            },
+            {
+              path: '/home',
+              component: '@/pages/index',
+            },
+            {
+              component: '@/pages/404',
+            },
+          ],
+        },
+        {
+          component: '@/pages/404',
+        },
+      ],
+    },
+    {
+      component: '@/pages/404',
+    },
   ],
 });
--- main/src/pages/index.tsx
+++ main/src/pages/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import styles from './index.less';
 
 export default () => {
-  return (
+  return (<>
     <div>
       <h1 className={styles.title}>Main</h1>
       <a href="/app1">App1</a>
     </div>
-  );
+  </>);
 }
